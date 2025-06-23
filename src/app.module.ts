859a1f539e290a8c083b85a7a1e9d213315c9b80
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2025-06-23 11:13:59 -0300

    Creating feature modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { CoursesModule } from './courses/courses.module';
+import { EnrollmentsModule } from './enrollments/enrollments.module';
+import { ClassesModule } from './classes/classes.module';
+import { GradesModule } from './grades/grades.module';
+import { SchedulesModule } from './schedules/schedules.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, CoursesModule, EnrollmentsModule, ClassesModule, GradesModule, SchedulesModule],
   controllers: [AppController],
   providers: [AppService],
 })

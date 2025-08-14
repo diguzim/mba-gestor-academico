186d5ee884d04992083e53f38ebf08db2f73c059
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2025-08-14 20:21:16 -0300

    Removing unused modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,6 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { CoursesModule } from './courses/courses.module';
 import { EnrollmentsModule } from './enrollments/enrollments.module';
-import { ClassesModule } from './classes/classes.module';
-import { GradesModule } from './grades/grades.module';
-import { SchedulesModule } from './schedules/schedules.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { NotificationsModule } from './notifications/notifications.module';
@@ -18,9 +15,6 @@ import { NotificationsModule } from './notifications/notifications.module';
     UsersModule,
     CoursesModule,
     EnrollmentsModule,
-    ClassesModule,
-    GradesModule,
-    SchedulesModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
